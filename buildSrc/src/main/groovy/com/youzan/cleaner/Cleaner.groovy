27f992d0a493c39e4d6e7f854eb0dcc04530b5da
liangfei <njliangfei@gmail.com>
2016-08-22 19:24:19 +0800

    create buildSrc

---
+++ buildSrc/src/main/groovy/com/youzan/cleaner/Cleaner.groovy
@@ -0,0 +1,22 @@
+package com.youzan.cleaner
+
+import org.gradle.api.Action
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task;
+
+/**
+ * Created by liangfei on 8/22/16.
+ */
+
+public class Cleaner implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.tasks.create("cleanResource", new Action<Task>() {
+            @Override
+            void execute(Task task) {
+                println "clean resource"
+            }
+        })
+    }
+}
