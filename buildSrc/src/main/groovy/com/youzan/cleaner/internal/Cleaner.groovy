5e0b960bd7647f6cec94a0a22aebd7198679c234
liangfei <njliangfei@gmail.com>
2016-08-29 10:50:37 +0800

    done

--- buildSrc/src/main/groovy/com/youzan/cleaner/internal/Cleaner.groovy
+++ buildSrc/src/main/groovy/com/youzan/cleaner/internal/Cleaner.groovy
@@ -1,8 +1,36 @@
 package com.youzan.cleaner.internal
 
-public class Cleaner {
-    def parse(File report) {
-        def issues = new XmlParser().parse(report)
+import groovy.xml.XmlUtil
 
+public class Cleaner {
+    def static clean(File report, Iterable<String> excludedFiles) {
+        def issues = new XmlSlurper().parse(report)
+        issues.'*'.findAll {
+            it.name() == 'issue' && it.@id == 'UnusedResources'
+        }.each {
+            def file = new File(it.location.@file.text())
+            if (file.name in excludedFiles) return;
+            def line = it.location.@line
+            def column = it.location.@column
+
+            if ((line == '' && column == '') || column == '1') {
+                println "deleting " + file.path
+                file.delete()
+            } else {
+                def m = it.@message =~ $/`R.(\w+).([^`]+)`/$
+                if (!m) return;
+
+                def type = m.group(1)
+                def entryName = m.group(2);
+
+                def parsed = new XmlSlurper().parse(file)
+                parsed.'**'.findAll {
+                    it.@name == entryName && it.name().contains(type)
+                }*.replaceNode {}
+
+                XmlUtil.serialize(parsed, new FileWriter(file))
+            }
+        }
     }
 }
+
